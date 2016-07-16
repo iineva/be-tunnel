a01304fdda98b2b8039b779a23947069bfccb53a
Steven <qzs2147@gamil.com>
2016-07-17 02:11:25 +0800

    服务端支持客户端注册

--- client-site.js
+++ client-site.js
@@ -8,8 +8,19 @@ program
   .option('-P, --tunnel-port <number>', 'tunnel port [2121]', 2121)
   .option('-h, --backend-host <string>', 'backend host [127.0.0.1]', '127.0.0.1')
   .option('-p, --backend-port <number>', 'backend port [80]', 80)
+  .option('-s, --public-host <string>', 'public host [127.0.0.1]', '127.0.0.1')
+  .option('-t, --public-port <number>', 'public port [80]', 80)
+  .option('-n, --service-name <string>', 'service name [client]', 'client')
   .parse(process.argv);
 
 
 // 启动服务
-client(program.tunnelHost, program.tunnelPort, program.backendHost, program.backendPort);
+client({
+    tunnelHost: program.tunnelHost,
+    tunnelPort: program.tunnelPort,
+    backendHost: program.backendHost,
+    backendPort: program.backendPort,
+    publicHost: program.publicHost,
+    publicPort: program.publicPort,
+    serviceName: program.serviceName,
+});
