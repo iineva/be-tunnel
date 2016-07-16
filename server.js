c56d70aff4186439f165e6145270288c39f7517b
Steven <qzs2147@gamil.com>
2016-07-16 13:29:54 +0800

    修复服务崩溃问题

--- server.js
+++ server.js
@@ -10,13 +10,20 @@ var PORT = 3001; // be-tunnel使用的端口
 var P = 3000; // 外部访问的端口
 
 var connects = []; // 保存链接
+var client = null; // 客户端服务器
 
 // 创建一个TCP服务器实例，调用listen函数开始监听指定端口
 // 传入net.createServer()的回调函数将作为”connection“事件的处理函数
 // 在每一个“connection”事件中，该回调函数接收到的socket对象是唯一的
 net.createServer(function(sock) {
 
-    net.createServer(function(s) {
+    if (client) {
+        console.log('❌be-tunnel重复链接');
+        sock.destroy();
+        return;
+    }
+
+    client = net.createServer(function(s) {
         // 转发用户的请求
         console.log('客户端链接: ' + s.remoteAddress + ':' + s.remotePort);
 
@@ -62,6 +69,12 @@ net.createServer(function(sock) {
     // 为这个socket实例添加一个"close"事件处理函数
     sock.on('close', function(data) {
         console.log('be-tunnel客户端断开');
+
+        // 销毁客户端服务器
+        client.close();
+        client = null;
+
+        // 清理所有链接
         for(var id in connects){
             connects[id].destroy();
         }
