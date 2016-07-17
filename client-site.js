2bb0525c156457bcd9de0e25f4d543e1fc1cacb2
Steven <qzs2147@gamil.com>
2016-07-17 13:48:14 +0800

    Update command.

--- client-site.js
+++ client-site.js
@@ -4,12 +4,12 @@ var client = require('./lib/client');
 
 program
   .version('0.0.1')
-  .option('-H, --tunnel-host <string>', 'tunnel host [127.0.0.1]', '127.0.0.1')
-  .option('-P, --tunnel-port <number>', 'tunnel port [2121]', 2121)
-  .option('-h, --backend-host <string>', 'backend host [127.0.0.1]', '127.0.0.1')
-  .option('-p, --backend-port <number>', 'backend port [80]', 80)
+  .option('-t, --tunnel-host <string>', 'tunnel host [127.0.0.1]', '127.0.0.1')
+  .option('-p, --tunnel-port <number>', 'tunnel port [2121]', 2121)
+  .option('-b, --backend-host <string>', 'backend host [127.0.0.1]', '127.0.0.1')
+  .option('-q, --backend-port <number>', 'backend port [80]', 80)
   .option('-s, --public-host <string>', 'public host [127.0.0.1]', '127.0.0.1')
-  .option('-t, --public-port <number>', 'public port [80]', 80)
+  .option('-w, --public-port <number>', 'public port [80]', 80)
   .option('-n, --service-name <string>', 'service name [client]', 'client')
   .parse(process.argv);
 
--- server-site.js
+++ server-site.js
@@ -3,12 +3,10 @@ var server = require('./lib/server');
 
 program
   .version('0.0.1')
-  .option('-H, --tunnel-host <string>', 'tunnel host [127.0.0.1]', '127.0.0.1')
-  .option('-P, --tunnel-port <number>', 'tunnel port [2121]', 2121)
-  .option('-h, --server-host <string>', 'server host [127.0.0.1]', '127.0.0.1')
-  .option('-p, --server-port <number>', 'server port [80]', 80)
+  .option('-t, --tunnel-host <string>', 'tunnel host [127.0.0.1]', '127.0.0.1')
+  .option('-p, --tunnel-port <number>', 'tunnel port [2121]', 2121)
   .parse(process.argv);
 
 
 // 启动服务
-server(program.tunnelHost, program.tunnelPort, program.serverHost, program.serverPort);
+server(program.tunnelHost, program.tunnelPort);
