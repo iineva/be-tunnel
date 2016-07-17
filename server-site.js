fb40c8ad0d5d87874c76f42064172e4f3a41b6f6
Steven <qzs2147@gamil.com>
2016-07-17 13:56:36 +0800

    Update default host.

--- server-site.js
+++ server-site.js
@@ -3,7 +3,7 @@ var server = require('./lib/server');
 
 program
   .version('0.0.1')
-  .option('-t, --tunnel-host <string>', 'tunnel host [127.0.0.1]', '127.0.0.1')
+  .option('-t, --tunnel-host <string>', 'tunnel host [0.0.0.0]', '0.0.0.0')
   .option('-p, --tunnel-port <number>', 'tunnel port [2121]', 2121)
   .parse(process.argv);
 
