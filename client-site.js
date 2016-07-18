a14ac530f0f23607458d878c30a5a87e3390e3a0
Steven <qzs2147@gamil.com>
2016-07-18 18:31:47 +0800

    添加环境变量传参

--- client-site.js
+++ client-site.js
@@ -1,17 +1,17 @@
 var program = require('commander');
 var client = require('./lib/client');
 
-
 program
   .version('0.0.1')
-  .option('-t, --tunnel-host <string>', 'tunnel host [127.0.0.1]', process.env.NODE_ENV.TUNNEL_HOST || '127.0.0.1')
-  .option('-p, --tunnel-port <number>', 'tunnel port [2121]', process.env.NODE_ENV.TUNNEL_PORT || 2121)
-  .option('-b, --backend-host <string>', 'backend host [127.0.0.1]', process.env.NODE_ENV.BACKEND_HOST || '127.0.0.1')
-  .option('-q, --backend-port <number>', 'backend port [80]', process.env.NODE_ENV.BACKEND_PORT || 80)
-  .option('-s, --public-host <string>', 'public host [0.0.0.0]', process.env.NODE_ENV.PUBLIC_HOST || '0.0.0.0')
-  .option('-w, --public-port <number>', 'public port [80]', process.env.NODE_ENV.PUBLIC_PORT || 80)
-  .option('-n, --service-name <string>', 'service name [client]', process.env.NODE_ENV.SERVICE_NAME || 'client')
+  .option('-t, --tunnel-host <string>', 'tunnel host [127.0.0.1]', process.env.TUNNEL_HOST || '127.0.0.1')
+  .option('-p, --tunnel-port <number>', 'tunnel port [2121]', process.env.TUNNEL_PORT || 2121)
+  .option('-b, --backend-host <string>', 'backend host [127.0.0.1]', process.env.BACKEND_HOST || '127.0.0.1')
+  .option('-q, --backend-port <number>', 'backend port [80]', process.env.BACKEND_PORT || 80)
+  .option('-s, --public-host <string>', 'public host [0.0.0.0]', process.env.PUBLIC_HOST || '0.0.0.0')
+  .option('-w, --public-port <number>', 'public port [80]', process.env.PUBLIC_PORT || 80)
+  .option('-n, --service-name <string>', 'service name [client]', process.env.SERVICE_NAME || 'client')
   .parse(process.argv);
+
 
 
 // 启动服务
