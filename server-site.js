4e050e0106cc576276acc031f9a233121f9e3a2e
Steven <qzs2147@gamil.com>
2016-07-18 18:21:07 +0800

    添加环境变量传参

--- server-site.js
+++ server-site.js
@@ -3,8 +3,8 @@ var server = require('./lib/server');
 
 program
   .version('0.0.1')
-  .option('-t, --tunnel-host <string>', 'tunnel host [0.0.0.0]', '0.0.0.0')
-  .option('-p, --tunnel-port <number>', 'tunnel port [2121]', 2121)
+  .option('-t, --tunnel-host <string>', 'tunnel host [0.0.0.0]', process.env.NODE_ENV.TUNNEL_HOST || '0.0.0.0')
+  .option('-p, --tunnel-port <number>', 'tunnel port [2121]', process.env.NODE_ENV.TUNNEL_PORT || 2121)
   .parse(process.argv);
 
 
